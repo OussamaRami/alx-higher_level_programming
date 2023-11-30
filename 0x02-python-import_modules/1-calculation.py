9ec18d2752cb4326602d9b68ef44d84154a2f24d
Your Name <you@example.com>
2023-11-30 07:53:31 -0800

    python import and modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+print("{} + {} = {}".format(a, b, add(a, b)))
+
+a = 10
+b = 5
+print("{} - {} = {}".format(a, b, sub(a, b)))
+
+a = 10
+b = 5
+print("{} * {} = {}".format(a, b, mul(a, b)))
+
+a = 10
+b = 5
+print("{} / {} = {}".format(a, b, div(a, b)))
+
