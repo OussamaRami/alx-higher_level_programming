45464a1504051cfcf5db6c2700ba1a11a2baf4ce
Your Name <you@example.com>
2023-11-30 07:58:29 -0800

    python import and modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,16 +7,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-    a = 10
-    b = 5
     print("{} - {} = {}".format(a, b, sub(a, b)))
-
-    a = 10
-    b = 5
     print("{} * {} = {}".format(a, b, mul(a, b)))
-
-    a = 10
-    b = 5
     print("{} / {} = {}".format(a, b, div(a, b)))
 
