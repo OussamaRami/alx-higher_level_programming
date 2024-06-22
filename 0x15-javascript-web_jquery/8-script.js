67b558376a2d995f52053ea764e93ac985781181
OussamaRami <138824857+OussamaRami@users.noreply.github.com>
2024-06-22 12:16:36 +0100

    Delete 0x15-javascript-web_jquery directory

--- 0x15-javascript-web_jquery/8-script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $.get("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
-    var movies = data.results;
-    $.each(movies, function(index, movie) {
-      $("#list_movies").append("<li>" + movie.title + "</li>");
-    });
-  });
-});
-
--- 0x15-javascript-web_jquery/9-script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $.get("https://hellosalut.stefanbohacek.dev/?lang=fr", function(data) {
-    $("#hello").text(data.hello);
-  });
-});
-
